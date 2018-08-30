consuldemocracy/consuldemocracy

Refactor segment constant into a class method
Remove warning due to reloading constant SEGMENTS[1]

Makes the definition of segments with geozones a little cleaner. I
think it’s worth it, compared to the slight memory gain of using a
constant[3]

[1] lib/user_segments.rb:2: warning: already initialized constant
UserSegments::SEGMENTS

[2]
https://stackoverflow.com/questions/15903835/class-method-vs-constant-in
-ruby-rails#comment55921926_15903970

[3]
https://stackoverflow.com/questions/15903835/class-method-vs-constant-in
-ruby-rails#answer-15903970

lib/user_segments.rb
@@ -1,13 +1,16 @@
 class UserSegments
-  SEGMENTS = %w(all_users
-                administrators
-                proposal_authors
-                investment_authors
-                feasible_and_undecided_investment_authors
-                selected_investment_authors
-                winner_investment_authors
-                not_supported_on_current_budget
-                beta_testers) + Geozone.all.map(&:name).map(&:parameterize).map(&:underscore).sort - ["city"]
+
+  def self.segments
+    %w(all_users
+       administrators
+       proposal_authors
+       investment_authors
+       feasible_and_undecided_investment_authors
+       selected_investment_authors
+       winner_investment_authors
+       not_supported_on_current_budget
+       beta_testers) + geozones
+  end
 
   def self.all_users
     User.active
@@ -58,6 +61,10 @@ class UserSegments
                  voodoorai2000@gmail.com)
 
     User.where(email: testers)
+  end
+
+  def self.geozones
+    Geozone.pluck(:name).map(&:parameterize).map(&:underscore).sort - ["city"]
   end
 
   Geozone.all.each do |geozone|

xkill/consul

lib/user_segments.rb
@@ -1,12 +1,15 @@
 class UserSegments
-  SEGMENTS = %w(all_users
-                administrators
-                proposal_authors
-                investment_authors
-                feasible_and_undecided_investment_authors
-                selected_investment_authors
-                winner_investment_authors
-                not_supported_on_current_budget) + Geozone.all.map(&:name).map(&:parameterize).map(&:underscore).sort - ["city"]
+
+  def self.segments
+    %w(all_users
+       administrators
+       proposal_authors
+       investment_authors
+       feasible_and_undecided_investment_authors
+       selected_investment_authors
+       winner_investment_authors
+       not_supported_on_current_budget) + geozones
+  end
 
   def self.all_users
     User.active
@@ -47,6 +50,10 @@ class UserSegments
                   current_budget_investments.pluck(:id)
                 )
     )
+  end
+
+  def self.geozones
+    Geozone.pluck(:name).map(&:parameterize).map(&:underscore).sort - ["city"]
   end
 
   Geozone.all.each do |geozone|